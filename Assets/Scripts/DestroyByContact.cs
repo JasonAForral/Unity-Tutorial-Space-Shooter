a3fa3770fee1cae2fb3f4742fe45ef0e87543395
Tsinoyboi <ajsnoface@gmail.com>
2015-04-08 13:51:19 -0700

    Added Score
    l2private
    find tagged object
    update and display GUIText (component)

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,10 +4,27 @@ using System.Collections;
 public class DestroyByContact : MonoBehaviour {
     public GameObject explosion;
     public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (null != gameControllerObject)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        else
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+
+
+
+    }
 
     void OnTriggerEnter(Collider other) 
     {
-        
         if (other.tag == "Boundary")
         {
             return;
@@ -21,6 +38,8 @@ public class DestroyByContact : MonoBehaviour {
             //gameController.GameOver();
         
         }
+        
+        gameController.AddScore(scoreValue);
 
         Destroy(other.gameObject);
         Destroy(gameObject);
