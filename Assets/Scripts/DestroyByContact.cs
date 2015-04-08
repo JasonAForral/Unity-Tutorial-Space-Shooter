6996eb8ddaed54123c4a89b61aa9c03c6fea72dd
Tsinoyboi <ajsnoface@gmail.com>
2015-04-08 04:55:29 -0700

    Got up to sound and gameplay
    What probe 4 would be

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation); // asteroid explodes
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation); // player explodes
+            //gameController.GameOver();
+        
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
